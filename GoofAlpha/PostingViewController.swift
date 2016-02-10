3088a65238e672adc770a96cec2f948409280b28
Julian Lee <Julian@Julians-MacBook-Pro.local>
2016-02-09 20:21:18 -0800

    connected storyboard to VCs

---
+++ GoofAlpha/PostingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PostingViewController.swift
+//  GoofAlpha
+//
+//  Created by Julian Lee on 2/9/16.
+//  Copyright © 2016 mobilemakers. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PostingViewController: UIViewController {
+
+    let ref = Firebase(url: "https://goof-alpha-app.firebaseio.com/")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
